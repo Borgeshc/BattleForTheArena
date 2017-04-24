55975c9fab8026d1f3e40ce72bf6c99477fcb382
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-04-24 09:56:56 -0600

    Organized folder structure, added post processing, added new particle systems

---
+++ Assets/Game/Scripts/GameplayScripts/CharacterScripts/Movement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public float sprintSpeed;
+    public float rotationSpeed;
+
+    public static bool moving;
+
+    CharacterController cc;
+    float horizontal;
+    float vertical;
+    Vector3 direction;
+    Animator anim;
+    InputDevice inputDevice;
+
+	void Start ()
+    {
+        cc = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+	}
+
+    void Update()
+    {
+        inputDevice = InputManager.ActiveDevice;
+
+        horizontal = inputDevice.LeftStick.X;
+        vertical = inputDevice.LeftStick.Y;
+
+        direction = new Vector3(horizontal, 0, vertical);
+
+        if (direction != Vector3.zero)
+            moving = true;
+        else
+            moving = false;
+
+
+        if (!inputDevice.LeftStickButton)
+            Move();
+        else
+            Sprint();
+	}
+
+    void Move()
+    {
+        cc.SimpleMove(transform.forward * vertical * speed * Time.deltaTime);
+        cc.SimpleMove(transform.right * horizontal * speed * Time.deltaTime);
+
+        anim.SetFloat("Vertical", vertical);
+        anim.SetFloat("Horizontal", horizontal);
+    }
+
+    void Sprint()
+    {
+        cc.SimpleMove(transform.forward * vertical * sprintSpeed * Time.deltaTime);
+        cc.SimpleMove(transform.right * horizontal * sprintSpeed * Time.deltaTime);
+
+        anim.SetFloat("Vertical", vertical);
+        anim.SetFloat("Horizontal", horizontal);
+    }
+}
