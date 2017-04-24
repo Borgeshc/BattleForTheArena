7f9c9d6300957dafa771e31796838f4d90870b76
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-04-24 17:48:49 -0600

    Added blocking

---
+++ Assets/Game/Scripts/GameplayScripts/CharacterScripts/Block.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class Block : MonoBehaviour
+{
+    public static bool isBlocking;
+
+    Animator anim;
+    InputDevice inputDevice;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        inputDevice = InputManager.ActiveDevice;
+
+        if (inputDevice.LeftBumper && !Movement.moving && !Aim.isAiming)
+        {
+            isBlocking = true;
+            anim.SetBool("Block", true);
+        }
+        else
+        {
+            isBlocking = false;
+            anim.SetBool("Block", false);
+        }
+    }
+}
