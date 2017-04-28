50bc7ab3ca79cd62fbd3fcae0b25f0970e9792b2
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-04-27 19:36:13 -0600

    Added main menu

---
+++ Assets/BattleForTheArena/Scripts/UltilityScripts/LookAtObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtObject : MonoBehaviour
+{
+    public GameObject targetObject;
+    
+	void Update ()
+    {
+        transform.LookAt(targetObject.transform);
+	}
+}
---
+++ Assets/BattleForTheArena/Scripts/UltilityScripts/StoreLightingSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreLightingSettings : MonoBehaviour
+{
+    public Material skyboxMaterial;
+    public bool fog;
+    
+	void Start ()
+    {
+        RenderSettings.skybox = skyboxMaterial;
+        RenderSettings.fog = fog;
+	}
+}
